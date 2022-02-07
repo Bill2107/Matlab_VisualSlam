81a8cfcda70d10741c1be45b0203e450f198c51c
Billy Ballico <will.ballico@gmail.com>
2022-02-08 00:38:02 +1100

    DepthMapGenerration Completed

--- Scripts/Layer1_Processing/DepthMapGeneration.m
+++ Scripts/Layer1_Processing/DepthMapGeneration.m
@@ -3,3 +3,14 @@ disparityMap{2} = disparitySGM(Images{1,2},Images{2,2}, "DisparityRange", [0 10*
 
 S = size(disparityMap{1});
 
+for j = 1:2
+    [xi,yi] = meshgrid( 1:1:S(2), 1:1:S(1));
+    [x,y] = meshgrid(1:1:S(2), 1:1:S(1));
+    x = x.*(~isnan(disparityMap{j}));
+    y = y.*(~isnan(disparityMap{j}));
+    DM = disparityMap{j};
+    %DM(isnan(DM)) = 0;
+   % InterpolatedDM{j} = scatteredInterpolant(x(:),y(:),double(DM(:)));
+    InterpolatedDM{j} = griddata(x,y,double(DM),xi,yi);
+  %  InterpolatedDM{j} = imfill(disparityMap{j},8);
+end
--- Scripts/main.m
+++ Scripts/main.m
@@ -5,5 +5,8 @@ for k = 1:kBig
     m = 10;
     R = ceil(length(Images{1,1}(1,:))/10);
     run('Layer1_Processing\FeatureTracking.m');
-    imshow(Images{1,1});
+    run('Layer1_Processing\DepthMapGeneration.m');
+        imshow(InterpolatedDM{1}, [0, 10*8]);
+        colormap jet
+        colorbar
 end

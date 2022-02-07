a15f5f058df78f4cd6630fbb5b67ef8b664deb29
Billy Ballico <will.ballico@gmail.com>
2022-02-08 01:25:07 +1100

    update Robot script added

---
+++ Scripts/Layer4_UpdateRobot/UpdateRobot.m
@@ -0,0 +1,9 @@
+ if(X(1) > 0) % dont want huge changes in heading
+    Robot{k, 1} = Robot{k-1,1} + X(2);
+    a = Robot{k-1,1};
+    Robot{k, 2} = Robot{k-1, 2} + [sin(a), 0, cos(a)].*X(1);
+else
+    Robot{k, 1} = Robot{k-1,1} + X(2);
+    a = Robot{k-1,1};
+    Robot{k,2} = Robot{k-1,2} + [sin(a), 0, cos(a)].*0.001;
+end

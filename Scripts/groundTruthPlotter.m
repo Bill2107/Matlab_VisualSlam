a1718f5a5284a9fd3c2bc76c3ad04c5ff0fd3598
Billy Ballico <will.ballico@gmail.com>
2022-02-08 04:27:32 +1100

    edge case fix
    edge case where 0 points are detected is ignored so as to not crash the program

---
+++ Scripts/groundTruthPlotter.m
@@ -0,0 +1,4 @@
+run('Layer0_DataCollection_Setup\SetupSequence.m');
+for k = 2:kBig
+    
+end
--- Scripts/main.m
+++ Scripts/main.m
@@ -1,16 +1,21 @@
 run('Layer0_DataCollection_Setup\SetupSequence.m');
 for k = 2:kBig
+    tic
     run('Layer0_DataCollection_Setup\SetupFrame.m');
     n = 5;
     m = 10;
     R = ceil(length(Images{1,1}(1,:))/10);
     run('Layer1_Processing\FeatureTracking.m');
     run('Layer1_Processing\DepthMapGeneration.m');
-    run('Layer2_PointCloudGeneration\PointCloudGeneration.m');
-    run('Layer3_CameraTransfrom\DetermineCameraTransfrom.m');
+    if(length(MP{1})> 5)
+        run('Layer2_PointCloudGeneration\PointCloudGeneration.m');
+        run('Layer3_CameraTransfrom\DetermineCameraTransfrom.m');
+    else
+        X = [0,0];
+    end
     run('Layer4_UpdateRobot\UpdateRobot.m');
-    tic
     run('Layer4_UpdateRobot\plotRobot.m');
+    disp(k)
     toc
 end
 run("Layer5_ClearProgram\EndProgram.m");

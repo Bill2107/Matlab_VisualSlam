21684143670b642080470f8ffbc21decd83a2aa9
Billy Ballico <will.ballico@gmail.com>
2022-02-08 00:26:34 +1100

    Feature Tracking Implemented + some fixes with distorted images

--- Scripts/Layer0_DataCollection_Setup/SetupFrame.m
+++ Scripts/Layer0_DataCollection_Setup/SetupFrame.m
@@ -2,3 +2,8 @@ Images{1,1} = imread(sprintf('%s/image_0/%06d.png', SEQ_PATH, k));
 Images{2,1} = imread(sprintf('%s/image_1/%06d.png', SEQ_PATH, k));
 Images{1,2} = imread(sprintf('%s/image_0/%06d.png', SEQ_PATH, k+1));
 Images{2,2} = imread(sprintf('%s/image_1/%06d.png', SEQ_PATH, k+1));
+
+Images{1,1} = undistortImage(Images{1,1},cameraParams{1});
+Images{2,1} = undistortImage(Images{2,1},cameraParams{2});
+Images{1,2} = undistortImage(Images{1,2},cameraParams{1});
+Images{2,2} = undistortImage(Images{2,2},cameraParams{2});
--- Scripts/main.m
+++ Scripts/main.m
@@ -1,5 +1,9 @@
 run('Layer0_DataCollection_Setup\SetupSequence.m');
 for k = 1:kBig
     run('Layer0_DataCollection_Setup\SetupFrame.m');
+    n = 5;
+    m = 10;
+    R = ceil(length(Images{1,1}(1,:))/10);
+    run('Layer1_Processing\FeatureTracking.m');
     imshow(Images{1,1});
 end

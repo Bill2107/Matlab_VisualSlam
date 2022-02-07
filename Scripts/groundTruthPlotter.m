ca850002c159a08338cf00d1cd281adde5b71a2e
Billy Ballico <will.ballico@gmail.com>
2022-02-08 04:43:34 +1100

    added a plotter for the ground truth for verification

--- Scripts/groundTruthPlotter.m
+++ Scripts/groundTruthPlotter.m
@@ -1,4 +1,11 @@
-run('Layer0_DataCollection_Setup\SetupSequence.m');
-for k = 2:kBig
-    
+Path = '../../../data_odometry_poses/dataset/poses';
+L =importdata(sprintf('%s/00.txt',Path));
+hold on
+for k = 1:2000
+x = L(k,4);
+y = L(k,4*2);
+z = L(k,4*3);
+plot(x,z,'.','Color','Green');
+disp(k);
+pause(0.01);
 end
